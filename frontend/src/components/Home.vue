d658c28767c33d326e411084f73b4d6935dbeca3
Miguel Contardo <84202264+maxmigue@users.noreply.github.com>
2024-10-03 10:16:24 -0300

    Primer cambio frontend
    Primera versión de mejora del login, aún falta por mejorar.

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  
+<h1 style="text-align: center;margin-top: 50px;">Home</h1>
+
+</template>
+
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
