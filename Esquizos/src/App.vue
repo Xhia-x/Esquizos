d18095fd4a90143c617b52093c1e79a3211b3362
estudiantedesesperado <aufuenzalida20@alumnos.utalca.cl>
2024-10-06 13:30:00 -0300

    Ficha que se puede mover

--- Esquizos/src/App.vue
+++ Esquizos/src/App.vue
@@ -1,39 +1,60 @@
 <template>
-  <div id="app">
+<div id="app">
     <video src="./assets/video.mp4" autoplay="true" muted="true" loop="true"></video>
-    <router-view/>
-  </div>
+
+    <router-view />
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 body {
-  background: none;
-  position: relative;
-  margin: 0;
+    background: none;
+    position: relative;
+    margin: 0;
 }
 
 video {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  min-width: 100%;
-  min-height: 100%;
-  transform: translateX(calc((100% - 100vw) / 2));
-  z-index: -210;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    transform: translateX(calc((100% - 100vw) / 2));
+    z-index: -210;
 }
 
+/* Nuevo estilo para el botón */
+.nav {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    z-index: 1;
+}
+
+button {
+    padding: 10px 20px;
+    background-color: #3498db;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #2980b9;
+}
 </style>
--- Esquizos/src/router/index.js
+++ Esquizos/src/router/index.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/LoginPage.vue';
 import LevelSelector from '../components/LevelSelector.vue';
+import Ficha from '@/components/Ficha.vue';
+
 
 const routes = [
   { path: '/', name: 'Login', component: Login },
-  { path: '/level-selector', name: 'LevelSelector', component: LevelSelector }
+  { path: '/level-selector', name: 'LevelSelector', component: LevelSelector },
+  {path: '/fichas', name: 'Ficha', component: Ficha}
 ];
 
 const router = createRouter({
