5654690c5972766c9f791cf743e48b4f22796001
Yanko <yacuna20@alumnos.utalca.cl>
2024-10-07 10:30:58 -0300

    Arreglo de conexion

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,12 @@ app.listen(9992, function check(err){
     }
 });
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 
 async function connectToDatabase() {
