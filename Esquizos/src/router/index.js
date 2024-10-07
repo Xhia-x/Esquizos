c808780ae3c3c0a343a0c9ee7b6c3cc8ee3b3c9a
Yanko Acuña <yacuna20@alumnos.utalca.cl>
2024-10-06 22:11:21 -0300

    Cherry pick

--- Esquizos/src/router/index.js
+++ Esquizos/src/router/index.js
@@ -1,14 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../components/LoginPage.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../components/LoginPage.vue'
+import CrearPartida from '@/components/CrearPartida.vue'
+import ConfigurarPartida from '@/components/ConfigurarPartida.vue'
+import VerPartida from '@/components/VerPartida.vue'
 import LevelSelector from '../components/LevelSelector.vue';
 import Ficha from '@/components/Ficha.vue';
 
-
 const routes = [
   { path: '/', name: 'Login', component: Login },
-  { path: '/level-selector', name: 'LevelSelector', component: LevelSelector },
+  {path: '/crearPartida', name: 'CrearPartida', component: CrearPartida},
+  {path: '/configurarPartida', name: 'ConfigurarPartida', component: ConfigurarPartida},
+  {path: '/partida/:nombrePartida', name: 'VerPartida', component: VerPartida},
+  {path: '/level-selector', name: 'LevelSelector', component: LevelSelector },
   {path: '/fichas', name: 'Ficha', component: Ficha}
-];
+]
 
 const router = createRouter({
   history: createWebHistory(),
