cf96ff67b2ad5935bc4906d1eaac0aeaf931d57c
Xhiah <ifarias21@alumnos.utalca.cl>
2024-10-06 22:04:01 -0300

    temporal

--- Esquizos/src/router/index.js
+++ Esquizos/src/router/index.js
@@ -2,12 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/LoginPage.vue';
 import LevelSelector from '../components/LevelSelector.vue';
 import Ficha from '@/components/Ficha.vue';
+import MonopolyView from '@/views/tablero.vue';
+
 
 
 const routes = [
   { path: '/', name: 'Login', component: Login },
   { path: '/level-selector', name: 'LevelSelector', component: LevelSelector },
-  {path: '/fichas', name: 'Ficha', component: Ficha}
+  { path: '/fichas', name: 'Ficha', component: Ficha},
+  { path: '/tablero', name: 'Tablero', component: MonopolyView}
 ];
 
 const router = createRouter({
