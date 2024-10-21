40e3acf4a020dcea13231c66d9f2916e38e3344e
Xpell <321seba1retamal2r@gmail.com>
2024-10-21 01:31:01 -0300

    Guardado de Posición en jugador

---
+++ Backend/src/jugador/jugadorController.js
@@ -0,0 +1,43 @@
+// jugadorController.js
+var jugadorService = require('./jugadorService.js');
+
+var subirJugadorControllerFn = async (req, res) => {
+    try {
+        console.log("Datos recibidos en el backend:", req.body); // Verifica los datos
+
+        var status = await jugadorService.subirJugadorDBService(req.body);
+        console.log("Resultado del servicio:", status);
+
+        if (status) {
+            res.send({ "status": true, "message": "Jugador almacenado correctamente" });
+        } else {
+            res.send({ "status": false, "message": "No se pudo almacenar el jugador" });
+        }
+    } catch (err) {
+        console.error("Error al subir el jugador:", err);
+        res.status(500).send({ "status": false, "message": "Error en el servidor" });
+    }
+};
+
+var actualizarJugadorControllerFn = async (req, res) => {
+    try {
+        const status = await jugadorService.actualizarJugadorDBService(req.body);
+
+        if (status) {
+            res.send({ "status": true, "message": "Jugador actualizado correctamente" });
+        } else {
+            res.send({ "status": false, "message": "No se pudo actualizar el jugador" });
+        }
+    } catch (err) {
+        console.error("Error al actualizar el jugador:", err);
+        res.status(500).send({ "status": false, "message": "Error en el servidor" });
+    }
+};
+
+
+
+// Exporta ambas funciones correctamente
+module.exports = {
+    subirJugadorControllerFn,
+    actualizarJugadorControllerFn
+};
---
+++ Esquizos/src/models/jugador.js
@@ -0,0 +1,9 @@
+export default class Partida {
+    constructor(userSchema, CasillaID,dinero,propiedades,tokenID) {
+        this.userSchema = userSchema;
+        this.CasillaID = CasillaID;
+        this.dinero = dinero;
+        this.propiedades = propiedades;
+        this.tokenID = tokenID;
+    }
+}
